c5663abba62e91d1ee723d2127a88de596b474d0
FaizalAzmanOUM <73531671+FaizalAzmanOUM@users.noreply.github.com>
2021-06-06 00:06:18 +0800

    Update settings.py

--- cupcake/settings.py
+++ cupcake/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
