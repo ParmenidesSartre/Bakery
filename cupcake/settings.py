2f3548883e88a6c9a3ce13ac4222e756d9c4e9ae
FaizalAzmanOUM <73531671+FaizalAzmanOUM@users.noreply.github.com>
2021-06-06 00:18:15 +0800

    Update settings.py

--- cupcake/settings.py
+++ cupcake/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zjbeb!-o)zuov1p+&-1$l=#-&!&9e^)at5z02j%7s895*^bkdz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://demo-taipingbakery.herokuapp.com/']
+ALLOWED_HOSTS = ['demo-taipingbakery.herokuapp.com']
 
 
 # Application definition
